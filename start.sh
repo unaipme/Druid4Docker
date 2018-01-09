61c9ac50901e77ed9a7a479f8c73d808757a42c5
Unai P. Mendizabal <unai.perez@ikerlan.es>
2018-01-09 12:24:18 +0100

    Minor improvements to Druid's start script

--- start.sh
+++ start.sh
@@ -1,45 +1,5 @@
 #!/bin/bash
 
-_COMP=$DRUID_COMPONENT
-
-if [ "$_COMP" == "" ]; then
-	echo "You must define which component to launch: DRUID_COMPONENT={historical, broker, coordinator, overlord, middleManager}"
-	exit 1
-fi
-if [ "$_COMP" != "historical" ] && [ "$_COMP" != "broker" ] && [ "$_COMP" != "coordinator" ] && [ "$_COMP" != "overlord" ] && [ "$_COMP" != "middleManager" ]; then
-	echo "Variable DRUID_COMPONENT must be one of {historical, broker, coordinator, overlord, middleManager}"
-	exit 1
-fi
-case $_COMP in
-	broker)
-		_MEMORY=${MEMORY-24g}
-		;;
-	coordinator)
-		_MEMORY=${MEMORY-3g}
-		;;
-	historical)
-		_MEMORY=${MEMORY-8g}
-		;;
-	middleManager)
-		_MEMORY=${MEMORY-64m}
-		;;
-	overlord)
-		_MEMORY=${MEMORY-3g}
-		;;
-esac
-
-_HDFS_CONF_DIR=${HDFS_CONF_DIR-/etc/hadoop/conf}
-_CONF_DIR=conf/druid/_common
-_CONF_FILE=$_CONF_DIR/common.runtime.properties
-_JVM_ARG_FILE=conf/druid/$_COMP/jvm.config
-_EXTENSIONS_LOADLIST=("druid-kafka-eight" "druid-s3-extensions" "druid-histogram" "druid-datasketches" "druid-lookups-cached-global" "mysql-metadata-storage")
-if [ "$ZK_SERVICE_HOST" == "" ]; then
-	echo "Zookeeper hosts must be set: ZK_SERVICE_HOST=<host>:<port>[, <host2>:<port2>...]"
-	exit 1
-fi
-_ZK_SERVICE_HOST=$ZK_SERVICE_HOST
-_ZK_PATHS_BASE=${ZK_PATHS_BASE-/druid}
-
 function addext {
 	_EXTENSIONS_LOADLIST[${#_EXTENSIONS_LOADLIST[@]}]=$1
 }
@@ -56,20 +16,66 @@ function addarg {
 	_DEF_ARGS="$_DEF_ARGS$1\n"
 }
 
+function warn {
+	echo -e "\033[1;33m"$1"\033[0m"
+}
+
+function err {
+	echo -e "\033[1;31m"$1"\033[0m"
+}
+
+_COMP=$DRUID_COMPONENT
+case $_COMP in
+	broker)
+		_MEMORY=${MEMORY-24g}
+		;;
+	coordinator)
+		_MEMORY=${MEMORY-3g}
+		;;
+	historical)
+		_MEMORY=${MEMORY-8g}
+		;;
+	middleManager)
+		_MEMORY=${MEMORY-64m}
+		;;
+	overlord)
+		_MEMORY=${MEMORY-3g}
+		;;
+	"")
+		err "You must define which component to launch: DRUID_COMPONENT={historical, broker, coordinator, overlord, middleManager}"
+		exit 1
+		;;
+	*)
+		err "Variable DRUID_COMPONENT must be one of {historical, broker, coordinator, overlord, middleManager}"
+		exit 1
+		;;
+esac
+
+_HDFS_CONF_DIR=${HDFS_CONF_DIR-/etc/hadoop/conf}
+_CONF_DIR=conf/druid/_common
+_CONF_FILE=$_CONF_DIR/common.runtime.properties
+_JVM_ARG_FILE=conf/druid/$_COMP/jvm.config
+_EXTENSIONS_LOADLIST=("druid-kafka-eight" "druid-s3-extensions" "druid-histogram" "druid-datasketches" "druid-lookups-cached-global" "mysql-metadata-storage")
+if [ "$ZK_SERVICE_HOST" == "" ]; then
+	err "Zookeeper hosts must be set: ZK_SERVICE_HOST=<host>:<port>[, <host2>:<port2>...]"
+	exit 1
+fi
+_ZK_SERVICE_HOST=$ZK_SERVICE_HOST
+_ZK_PATHS_BASE=${ZK_PATHS_BASE-/druid}
 if [ "$METADATA_URI" == "" ] && [ "$METADATA_HOST" == "" ]; then
-	echo "Either the whole metadata database URI or the host must be defined."
-	echo "To define the whole URI:"
-	echo "METADATA_URI=jdbc:<db_type>://<db_host>:<db_port>/<db_name>[?options...]"
-	echo "Take advantage of the default values by setting only the host:"
-	echo "METADATA_HOST=<hostname or ip of db>"
-	echo "METADATA_PORT=<db port>, defaults to 3306 for MySQL, 5432 for PostgreSQL and 1527 for Derby"
-	echo "METADATA_DBNAME=<db name>, defaults to druid"
+	err "Either the whole metadata database URI or the host must be defined."
+	err "To define the whole URI:"
+	err "METADATA_URI=jdbc:<db_type>://<db_host>:<db_port>/<db_name>[?options...]"
+	err "Take advantage of the default values by setting only the host:"
+	err "METADATA_HOST=<hostname or ip of db>"
+	err "METADATA_PORT=<db port>, defaults to 3306 for MySQL, 5432 for PostgreSQL and 1527 for Derby"
+	err "METADATA_DBNAME=<db name>, defaults to druid"
 	exit 1
 elif [ "$METADATA_USER" == "" ]; then
-	echo "The username for the metadata database must be set. METADATA_USER=<username>"
+	err "The username for the metadata database must be set. METADATA_USER=<username>"
 	exit 1
 elif [ "$METADATA_PASSWORD" == "" ]; then
-	echo "The password for the metadata database must be set. METADATA_PASSWORD=<password>"
+	err "The password for the metadata database must be set. METADATA_PASSWORD=<password>"
 	exit 1
 fi
 
@@ -94,16 +100,16 @@ case $METADATA_TYPE in
 		_METADATA_PASSWORD=$METADATA_PASSWORD
 		;;
 	derby)
-		echo "Only viable in a cluster with a single Coordinator, no fail-over. Do not use Derby on production"
+		warn "Using Derby is only viable in a cluster with a single Coordinator, no fail-over. Do not use Derby on production"
 		_METADATA_TYPE=derby
 		_METADATA_URI=${METADATA_URI-jdbc:derby://$METADATA_HOST:${METADATA_PORT-1527}/${DERBY_DBPATH-var/druid/metadata.db};create=true}
 		;;
 	"")
-		echo "You have not set any metadata storage type. METADATA_TYPE={mysql, postgresql, derby}."
+		err "You have not set any metadata storage type. METADATA_TYPE={mysql, postgresql, derby}."
 		exit 1
 		;;
 	*)
-		echo "The metadata storage type ($METADATA_TYPE) you chose is not supported. METADATA_TYPE={mysql, postgresql, derby}."
+		err "The metadata storage type ($METADATA_TYPE) you chose is not supported. METADATA_TYPE={mysql, postgresql, derby}."
 		exit 1
 		;;
 esac
@@ -113,9 +119,9 @@ case $STORAGE_TYPE in
 		_STORAGE_TYPE=hdfs
 		_STORAGE_DIR=${STORAGE_DIR-/druid/segments}
 		if [ ! -f $_HDFS_CONF_DIR/core-site.xml ] && [ ! -f $_HDFS_CONF_DIR/hdfs-site.xml ]; then
-			echo "To use HDFS as the deep storage, files core-site.xml and hdfs-site.xml are needed."
-			echo "These files are searched for in the directory set with variable HDFS_CONF_DIR, which defaults to /etc/hadoop/conf"
-			echo "Create a volume that points to the directory where the xml files are."
+			err "To use HDFS as the deep storage, files core-site.xml and hdfs-site.xml are needed."
+			err "These files are searched for in the directory set with variable HDFS_CONF_DIR, which defaults to /etc/hadoop/conf"
+			err "Create a volume that points to the directory where the xml files are."
 			exit 1
 		else
 			cp $_HDFS_CONF_DIR/core-site.xml $_CONF_DIR
@@ -123,20 +129,20 @@ case $STORAGE_TYPE in
 		fi
 		;;
 	s3)
-		echo "Not yet supported"
+		err "Not yet supported"
 		exit 1
 		;;
 	local)
-		echo "Only viable in a cluster if this is a network mount"
+		warn "Using local segment storage is only viable in a cluster if this is a network mount"
 		_STORAGE_TYPE=local
 		_STORAGE_DIR=${STORAGE_DIR-var/druid/segments}
 		;;
 	"")
-		echo "You have not set any deep storage type"
+		err "You have not set any deep storage type. STORAGE_TYPE={hdfs, s3, local}"
 		exit 1
 		;;
 	*)
-		echo "The deep storaget type ($STORAGE_TYPE) you chose is not supported."
+		err "The deep storaget type ($STORAGE_TYPE) you chose is not supported. STORAGE_TYPE={hdfs, s3, local}"
 		exit 1
 		;;
 esac
